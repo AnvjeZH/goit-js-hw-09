8567bce89da48820ad149ca1b37c61b48948abe1
Anzhelika Zhupynas <121193138+AnvjeZH@users.noreply.github.com>
2023-05-31 19:17:27 +0300

    done task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const bodyRef = document.body;
 
+let timerId = null;
+
+startBtn.addEventListener('click', onChangeColor);
+stopBtn.addEventListener('click', onStopChangeColor);
+
+function onChangeColor() {
+  timerId = setInterval(() => {
+    bodyRef.style.backgroundColor = getRandomHexColor();
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+  }, 1000);
+}
+function onStopChangeColor() {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
